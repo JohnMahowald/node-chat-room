3d21b9c8c9cf90de8ae39d0c0b13206700a710c2
CrookedKings <John.Mahowald@gmail.com>
2014-11-24 11:04:34 -0800

    improved ui, styling, keypress handlers

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,14 +1,14 @@
 var socket = io();
 var chat = new Chat(socket);
 
-function getMessage(event) {
+function getMessage() {
   var message = $("#outgoing").val();
   return message;
 };
 
 function updateGroupMessage(message) {
   var $li = $("<li>");
-  $li.text(message.currentRoom + " | " + message.user + ": " + message.text);
+  $li.html("<strong>" + message.user + "</strong>" + ": " + message.text);
   $("#group-messages").append($li);
   $("#incoming").scrollTop(1000);
 }
@@ -20,18 +20,32 @@ function postCurrentStatus(response) {
   var $li2 = $("<li>");
   $li2.text(response.message);
   $("#group-messages").append($li2);
+	$("#current-room").html(response.currentRoom);
 }
 
 $(document).ready(function(){
+	$("#outgoing").on("keypress", function(e) {
+		if (e.keyCode == 13) {
+			processOutgoingMessage()
+			setTimeout( function() {
+				$("#outgoing").val("")
+			}, 0)
+		}
+	})
+	
   $("#message-submit").on("click", function(event) {
     event.preventDefault();
-    var message = getMessage(event);
+		processOutgoingMessage();
+  })
+	
+	function processOutgoingMessage (event) {
+    var message = getMessage();
     var commandRegex = /\//;
     if (commandRegex.exec(message)) {
       chat.processCommand(message)
     } else {
       chat.sendMessage(message);
     }
-    $("#outgoing").val("");
-  })
+		$("#outgoing").val("").focus()	
+	}
 });
